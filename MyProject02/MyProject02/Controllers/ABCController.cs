759c0454e12dcfe72d5afc332501264c6d646d02
n_whjang <n_whjang@naver.com>
2017-12-14 20:15:05 +0900

    프로젝트 등록

---
+++ MyProject02/MyProject02/Controllers/ABCController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyProject02.Controllers
+{
+    public class ABCController : Controller
+    {
+        // GET: ABC
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
